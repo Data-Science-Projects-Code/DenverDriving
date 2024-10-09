e2f283c3ea93e304328ab1f5c192169f958f7cb4
hrokr <alex.rook@gmail.com>
2024-10-09 02:14:24 -0400

    Major refactor of pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,95 +1,166 @@
 import pandas as pd
-from datetime import time
 
-df = pd.read_csv("../data/traffic_accidents.csv", low_memory=False)
-
-df.columns = [
-    "_".join(word.capitalize() for word in col.split("_")) for col in df.columns
+# List of values to replace directly in the code for simplicity
+VALUES_TO_REPLACE = [
+    "0",
+    "00",
+    " 00",
+    "  00",
+    "01",
+    "02",
+    "03",
+    "05",
+    " 06",
+    "07",
+    "17",
+    "18",
+    "021",
+    "91",
+    "Na",
+    "na",
+    "/",
+    " ",
+    "--",
+    "-",
 ]
 
-df.drop(
-    ["Last_Occurrence_Date", "Point_X", "Point_Y", "Geo_X", "Geo_Y", "X", "Y"],
-    axis=1,
-    inplace=True,
-)
 
-date_cols = [col for col in df.columns if "Date" in col]
+def load_data():
+    """Attempts to load data from various sources with a fallback to an empty DataFrame."""
+    file_paths = [
+        "../data/traffic_accidents.csv",
+        # "../data/traffic_accidents.parquet",
+        # "/kaggle/input/denver-traffic-accidents/denver_accidents.parquet",
+        # "/kaggle/input/denver-traffic-accidents/traffic_accidents.csv",
+    ]
 
-int_cols = [col for col in df.select_dtypes(include="int64").columns]
+    for path in file_paths:
+        try:
+            if path.endswith(".parquet"):
+                return pd.read_parquet(path), f"Parquet file loaded from {path}"
+            elif path.endswith(".csv"):
+                return pd.read_csv(
+                    path, low_memory=False
+                ), f"CSV file loaded from {path}"
+        except FileNotFoundError:
+            continue
 
-cat_cols = [
-    "Offense_Code",
-    "Offense_Code_Extension",
-    "Top_Traffic_Accident_Offense",
-    "District_Id",
-    "Precinct_Id",
-    "Bicycle_Ind",
-    "Pedestrian_Ind",
-    "Harmful_Event_Seq_1",
-    "Harmful_Event_Seq_2",
-    "Harmful_Event_Seq_3",
-    "Road_Location",
-    "Road_Description",
-    "Road_Contour",
-    "Road_Condition",
-    "Light_Condition",
-    "Tu1_Vehicle_Type",
-    "Tu1_Travel_Direction",
-    "Tu1_Vehicle_Movement",
-    "Tu1_Driver_Action",
-    "Tu1_Driver_Humancontribfactor",
-    "Tu1_Pedestrian_Action",
-    "Tu2_Vehicle_Type",
-    "Tu2_Travel_Direction",
-    "Tu2_Vehicle_Movement",
-    "Tu2_Driver_Action",
-    "Tu2_Driver_Humancontribfactor",
-    "Tu2_Pedestrian_Action",
-    "Seriously_Injured",
-    "Fatality_Mode_1",
-    "Fatality_Mode_2",
-    "Seriously_Injured_Mode_1",
-    "Seriously_Injured_Mode_2",
-]
+    return pd.DataFrame(), "File not found. Returning empty DataFrame."
 
-for col in date_cols:
-    df[col] = pd.to_datetime(df[col], format="%m/%d/%Y %I:%M:%S %p")
 
-for col in int_cols:
-    df[col] = df[col].astype("int32")
+def format_column_names(df):
+    """Converts column names to Title_Snake_Case format."""
+    df.columns = [
+        "_".join(word.capitalize() for word in col.split("_")) for col in df.columns
+    ]
+    return df
 
-for col in cat_cols:
-    df[col] = df[col].astype("category")
 
-df["Fatalities"] = pd.to_numeric(df["Fatalities"], errors="coerce")
+def drop_unused_columns(df):
+    """Drops columns that are not useful for analysis."""
+    unused_columns = [
+        "Object_Id",
+        "Last_Occurrence_Date",
+        "Geo_X",
+        "Geo_Y",
+        "Point_X",
+        "Point_Y",
+        "X",
+        "Y",
+    ]
+    return df.drop(unused_columns, axis=1, errors="ignore")
 
-# Overwrite NaNs
-field_not_entered_cols = [
-    "Seriously_Injured_Mode_1",
-    "Seriously_Injured_Mode_2",
-    "Tu1_Pedestrian_Action",
-    "Tu2_Vehicle_Type",
-    "Tu2_Travel_Direction",
-    "Tu2_Vehicle_Movement",
-    "Tu2_Driver_Action",
-    "Tu2_Driver_Humancontribfactor",
-    "Tu2_Pedestrian_Action",
-]
-pending_investigation_cols = [
-    "Fatality_Mode_1",
-    "Fatality_Mode_2",
-    "Tu1_Vehicle_Movement",
-    "Incident_Address",
-    "Road_Location",
-    "Road_Condition",
-]
-minus_one_cols = ["Bicycle_Ind", "Pedestrian_Ind"]
 
-df[field_not_entered_cols] = df[field_not_entered_cols].fillna("Field Not Entered")
+def convert_to_datetime(df):
+    """Converts columns containing 'Date' in their name to datetime format."""
+    date_cols = [col for col in df.columns if "Date" in col]
+    for col in date_cols:
+        df[col] = pd.to_datetime(
+            df[col], format="%m/%d/%Y %I:%M:%S %p", errors="coerce"
+        )
+    return df
 
-# df[pending_investigation_cols] = df[pending_investigation_cols].fillna(
-#     "Pending Investigation and/or Court Hearing"
-# )
-# df[minus_one_cols] = df[minus_one_cols].fillna(-1).astype("int32")
 
-# df.to_parquet("../data/denver_accidents.parquet")
+def clean_data(df):
+    """Performs data cleaning operations on specific columns."""
+    df.dropna(subset=["Geo_Lon", "Geo_Lat"], axis=0, inplace=True)
+    df["Top_Traffic_Accident_Offense"] = (
+        df["Top_Traffic_Accident_Offense"].str.rstrip().replace("TRAF - ", "")
+    )
+    return df
+
+
+def fix_missing_data(df, number_cols, text_cols):
+    """Handles missing values and incorrect entries in the data."""
+    # Fill missing values in numerical columns
+    df[number_cols] = df[number_cols].fillna(-1)
+
+    # Fill missing values and handle special cases in text columns
+    df[text_cols] = (
+        df[text_cols]
+        .fillna("Data Misentered")
+        .replace(r"^\s*$", "Data Misentered", regex=True)
+    )
+    df[text_cols] = df[text_cols].replace(VALUES_TO_REPLACE, "Data Misentered")
+    return df
+
+
+def column_contents_to_title_case(df, columns):
+    """Converts the contents of specified columns to title case."""
+    for col in columns:
+        if col in df.columns:
+            df[col] = df[col].astype(str).str.title()
+    return df
+
+
+def convert_columns_to_category(df, category_cols):
+    """Converts specified columns to categorical data type."""
+    df[category_cols] = df[category_cols].astype("category")
+    return df
+
+
+def main():
+    df, load_message = load_data()
+    print(load_message)
+
+    df = (
+        df.pipe(format_column_names)
+        .pipe(drop_unused_columns)
+        .pipe(convert_to_datetime)
+        .pipe(clean_data)
+    )
+
+    number_cols = df.select_dtypes(include=["int", "float"]).columns.tolist()
+    text_cols = df.select_dtypes(include="object").columns.tolist()
+
+    df = fix_missing_data(df, number_cols, text_cols)
+
+    title_case_cols = [col for col in text_cols if "Id" not in col]
+    df = column_contents_to_title_case(df, title_case_cols)
+
+    additional_cols = [
+        "Offense_Code",
+        "Offense_Code_Extension",
+        "Precinct_Id",
+        "Bicycle_Ind",
+        "Pedestrian_Ind",
+        "Seriously_Injured",
+        "Fatalities",
+    ]
+
+    to_categorical = [
+        col for col in text_cols if col not in text_cols[1:2]
+    ] + additional_cols
+    df = convert_columns_to_category(df, to_categorical)
+
+    # Convert specified columns to float32 for optimized storage
+    df = df.astype(
+        {"Incident_Id": "float32", "Geo_Lon": "float32", "Geo_Lat": "float32"}
+    )
+
+    # Save the processed DataFrame to a parquet file
+    df.to_parquet("../data/denver_accidents.parquet")
+
+
+if __name__ == "__main__":
+    main()
